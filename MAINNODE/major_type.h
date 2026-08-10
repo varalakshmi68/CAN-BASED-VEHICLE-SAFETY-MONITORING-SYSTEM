6a536e6c8d52bf35712195f2a480681c33839aed
varalakshmi68 <varalakshmmiakulaa68@gmail.com>
2026-08-10 20:20:05 +0530

    Add header comment to major_type.h
    Add header comment to major_type.h for clarity.

---
+++ MAINNODE/major_type.h
@@ -0,0 +1,9 @@
+// Defines short and readable data type names used throughout the project.
+typedef unsigned char u8;
+typedef char s8;
+typedef unsigned int u32;
+typedef int s32;
+typedef unsigned short int u16;
+typedef signed short int s16;
+typedef float f32;
+typedef double d64;
