55a1d796ce1b88a3667b544216a194b9bdb0eb6d
varalakshmi68 <varalakshmmiakulaa68@gmail.com>
2026-08-10 19:51:44 +0530

    Improve header guard comments in buzzer_major.h
    Added comments to clarify header guard usage.

---
+++ MAINNODE/buzzer_major.h
@@ -0,0 +1,8 @@
+#ifndef _BUZZER_H_    //prevents the header from being included more than once.
+#define  _BUZZER_H_   //creates the header guard.
+
+void Buzzer_init(void);
+void Buzzer_ON(void);
+void Buzzer_OFF(void);
+
+#endif                //ends the header guard.
